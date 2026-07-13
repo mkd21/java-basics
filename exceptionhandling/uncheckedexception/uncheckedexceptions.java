d893538f0f436eb17e809a256fc63a22af64375e
Mayank Deep <mayankdeep21@MacBook-Air.local>
2026-07-14 02:15:54 +0530

    taking user input

--- exceptionhandling/uncheckedexception/uncheckedexceptions.java
+++ exceptionhandling/uncheckedexception/uncheckedexceptions.java
@@ -1,13 +1,36 @@
 
 package exceptionhandling.uncheckedexception;
 
-class UncheckedExceptionsExample 
+class UncheckedExceptionsExample extends RuntimeException
 {
-    
+    public UncheckedExceptionsExample(){}
+
+    public UncheckedExceptionsExample(String message)
+    {
+        super(message);
+    }
 }
 
 public class uncheckedexceptions {
     public static void main(String[] args) {
         
+        int i = 23;
+
+        int j = 0;
+
+        if( j == 0) throw new UncheckedExceptionsExample("cannot divide by 0");
+
     }
 }
+
+
+/*
+
+    now we are extending RuntimeException class 
+
+    compiler don't force to handle exceptions
+    if anything unexpected happens then whole program will crash and we will need to fix that
+
+
+
+*/
